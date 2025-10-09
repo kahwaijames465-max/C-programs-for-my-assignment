0289609dddaf45dd1a60e5bbf1eef60ad64f8017
kahwaijames465-max <kahwaijames465@gmail.com>
2025-10-09 17:45:07 +0300

    Add files via upload

---
+++ DescendingNumbers.c
@@ -0,0 +1,18 @@
+/*
+Name:James Kahwai
+Reg no:CT101/G/26490/25
+Date:06/10/25
+Description:C program to print all numbers from 100 to 50 in descending order
+*/
+#include<stdio.h>
+
+int main()
+{
+     //variable declaration 
+     int i;
+     //loop display 
+     for(i=100; i>=50; i--){
+    printf("%d\n",i);
+    }
+    return 0;
+}
